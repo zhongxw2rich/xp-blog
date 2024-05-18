a242656470d9d4ab473178793d59c2a4dbcada77
zhongxw2rich <zhongxw2024@gmail.com>
2024-05-18 12:53:26 +0800

    前后端联调

--- blog-client/next.config.mjs
+++ blog-client/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://127.0.0.1:5000/:path*'
+            }
+        ]
+    }
+
+};
 
 export default nextConfig;
